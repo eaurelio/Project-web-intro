069d325c8781e90a44486dd6aef929708866aa4c
Edson <edson.exe@outlook.com>
2022-08-26 22:21:02 -0300

    Semana 6

---
+++ project/assets/js/script.js
@@ -0,0 +1,178 @@
+const principal = []
+const acompanhamento = []
+
+const pratos = [
+{
+    id: 1,
+    nome: 'Pizzas',
+    principal: true,
+    tamanho: ['P', 'M', 'G', 'F'],
+    sabor: [
+      'A moda da casa',
+      'Bacon',
+      'Calabresa',
+      'Franco c/ catupiry',
+      'Italiana',
+      'Portuquesa',
+      'Marguerita'
+    ],
+    doce: false
+  },
+  
+  {
+    id: 2,
+    nome: 'Lasanhas',
+    principal: true,
+    tamanho: ['P', 'M', 'G'],
+    sabor: [
+      'Bolonhesa',
+      'Quatro quejos',
+      'Franco c/ catupiry',
+      'Carne'
+    ],
+    doce: false
+  },
+  
+  {
+    id: 3,
+    nome: 'Spaghetti',
+    principal: true,
+    tamanho: ['Único'],
+    sabor: [
+      'Fettuccine Allegro',
+      'Massa Alfredo',
+      'Massa ao molho pomodoro',
+      'Massa Bolognesa Clássico',
+      'Massa Carmonara',
+      'Penne Caprese',
+      'Massa Italiaa com filé de frango',
+      'Al Mare',
+    ],
+    doce: false
+  },
+  
+  {
+    id: 4,
+    nome: 'Focaccia',
+    principal: true,
+    tamanho: ['Único'],
+    sabor: [
+      'Focaccia Calabresa',
+      'Focaccia Champgnon',
+      'Focaccia Marguerita',
+      'Focaccia Mussarela',
+      'Focaccia Queijos'
+    ]
+  },
+  
+  {
+    id: 5,
+    nome: 'Saladas',
+    principal: false,
+    tamanho: ['Único'],
+    sabor: [
+      'Bambini',
+      'Tradizionale',
+      'Caesar',
+    ]
+  },
+  
+  {
+    id: 6,
+    nome: 'Sobremesas',
+    principal: false,
+    tamanho: ['Único'],
+    sabor: [
+      'Cannoli',
+      'Cioccolato',
+      'Mil Folhas',
+      'Pudim'
+    ]
+  },
+  
+  {
+    id: 7,
+    nome: 'Vinhos',
+    principal: false,
+    tamanho: ['Único'],
+    sabor: [
+      'Cannoli',
+      'Cioccolato',
+      'Mil Folhas',
+      'Pudim'
+    ]
+  }
+]
+
+pratos.map(function (el, i, array) {
+  let sabores = el.sabor.toString()
+  el.sabor = sabores
+})
+
+
+pratos.map(function (el, i, array) {
+  if(el.principal === true) {
+    principal.push(el)
+  } else {
+    acompanhamento.push(el)
+  }
+})
+
+// console.log(principal)
+// console.log(acompanhamento)
+
+// *****************************************************************
+// Semana 5
+// for (i in principal) {
+//   console.log(principal[i])
+// }
+
+
+// *****************************************************************
+// Semana 6-1
+
+// const objetoTeste ={
+//   id: 1,
+//   nome: 'Pizzas',
+//   principal: true,
+//   tamanho: ['P', 'M', 'G', 'F'],
+//   sabor: [
+//     'A moda da casa',
+//     'Bacon',
+//     'Calabresa',
+//     'Franco c/ catupiry',
+//     'Italiana',
+//     'Portuquesa',
+//     'Marguerita'
+//   ],
+//   doce: false
+// }
+// const retornaObjeto = x => {
+//   let string
+//   for (let i in x) {
+//     string += x[i]
+//   }
+//   return string
+// }
+
+// console.log(retornaObjeto(objetoTeste))
+
+// *****************************************************************
+// Semana 6-2
+
+const retornaObjetoNome = (array, chave) => {
+  let obj
+  for (let i in array) {
+    if (array[i].nome === chave) {
+      // console.log(array[i])
+      obj = {...array[i]}
+      // console.log(obj)
+    } else if (obj === undefined){
+      obj = 'Não encontrado!'
+    }
+  }
+  return obj
+}
+
+// retornaObjetoNome(acompanhamento, 'Saladas')
+console.log(retornaObjetoNome(acompanhamento, 'Pizzas'))
